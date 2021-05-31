2cafaba093e733016796643c8aac0cdd42e13ea2
dragongt501 <61512241+dragongt501@users.noreply.github.com>
2021-05-31 17:09:19 +0700

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,92 @@
+﻿#include <Windows.h>
+#include <WinUser.h>
+
+#define FILE_MENU_NEW 1
+#define FILE_MENU_OPEN 2
+#define FILE_MENU_EXIT 3
+#define CHANGE_TITLE 4
+
+LRESULT CALLBACK WindowProcedure(HWND, UINT, WPARAM, LPARAM);
+void AddMenus(HWND);
+void AddControls(HWND);
+HMENU hMenu;
+HWND hEdit;
+
+
+int WINAPI WinMain(_In_ HINSTANCE hInst, _In_opt_ HINSTANCE hPrevInst, _In_ LPSTR args, _In_ int ncmdshow)
+{
+	WNDCLASSW wc = { 0 };
+	wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hInstance = hInst;
+	wc.lpszClassName = L"myWindowClass";
+	wc.lpfnWndProc = WindowProcedure;
+	if (!RegisterClassW(&wc))
+		return -1;
+	CreateWindowW(L"myWindowClass", L"My Window", WS_OVERLAPPEDWINDOW | WS_VISIBLE, 100, 100, 800, 450, NULL, NULL, NULL, NULL);
+	MSG msg = { 0 };
+	while (GetMessage(&msg, NULL, NULL, NULL))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return 0;
+}
+
+LRESULT CALLBACK WindowProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp)
+{
+	switch ( msg)
+	{
+	case WM_COMMAND:
+		switch (wp)
+		{
+		case FILE_MENU_EXIT:
+			DestroyWindow(hWnd);
+			break;
+		case FILE_MENU_NEW:
+			break;
+		case CHANGE_TITLE:
+			wchar_t text[100];
+			GetWindowText(hEdit, text, 100);
+			SetWindowTextW(hWnd, text);
+			break;
+		default:
+			break;
+		}
+	case WM_CREATE:
+		AddMenus(hWnd);
+		AddControls(hWnd);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProcW(hWnd, msg, wp, lp);
+	}
+}
+
+void AddMenus(HWND hWnd)
+{
+	hMenu = CreateMenu();
+	HMENU hFileMenu = CreateMenu();
+	HMENU hFileOpenSubMenu = CreateMenu();
+	AppendMenu(hFileOpenSubMenu, MF_STRING, CHANGE_TITLE, L"Change Title");
+	AppendMenu(hFileOpenSubMenu, MF_STRING, NULL, L"Item 2");
+	AppendMenu(hFileMenu, MF_STRING, FILE_MENU_NEW, L"New");
+	AppendMenu(hFileMenu, MF_POPUP, (UINT_PTR)hFileOpenSubMenu, L"Open");
+	AppendMenuW(hFileMenu, MF_SEPARATOR, NULL, NULL);
+	AppendMenu(hFileMenu, MF_STRING, FILE_MENU_EXIT, L"Exit");
+	AppendMenu(hMenu, MF_POPUP, (UINT_PTR) hFileMenu, L"File");
+	AppendMenu(hMenu, MF_STRING, NULL, L"Help");
+
+	SetMenu(hWnd, hMenu);
+
+
+}
+
+void AddControls(HWND hWnd)
+{
+	CreateWindow(L"static", L"Enter text here: ", WS_VISIBLE | WS_CHILD , 45,40, 200, 50, hWnd, NULL, NULL, NULL);
+	hEdit = CreateWindow(L"Edit", L"abc" , WS_VISIBLE | WS_CHILD | WS_BORDER | ES_MULTILINE | ES_AUTOHSCROLL | ES_AUTOVSCROLL, 45, 120, 200, 50, hWnd, NULL, NULL, NULL, NULL);
+}
